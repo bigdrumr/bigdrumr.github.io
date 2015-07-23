c6dc610471ef7e052cd08ece67cf033c8f66cccf
terrygriffin <jtgrffn@gmail.com>
2015-07-23 16:10:10 -0500

    new layout. First codepens.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+//PROGRESS BUTTON TRANSITION
+// $('.item-primary').click(function(){
+// 	$(this).removeClass('active');
+//   	$(this).addClass('active');
+// });
+
+//SAVE BUTTON TRANSITION
+// $('#save li').hover(
+//   function(){
+//   	$(this).addClass('above');
+//   	$('#save li').not('.above').addClass('inactive');
+// 	}, function(){
+//   	$(this).removeClass('above');
+//   	$('#save li').not('.above').removeClass('inactive');      
+// });
+
+// $('.save').hover(function(){    
+//   $('.save.option').toggleClass('active');
+// });
+
+// $('.save').hover(function(){
+// 	$('.complete.parent').css('transform','translate(72px)');
+// },function(){
+//   $('.complete.parent').css('transform','translate(0px)');
+// });
+
+// $('.complete').hover(function(){
+//   $('.complete.option').toggleClass('active');
+// });
+
