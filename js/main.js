bb117706f1d1b25e2a9de8fe9c61914253705f3b
terrygriffin <jtgrffn@gmail.com>
2015-08-01 17:08:07 -0500

    resume modal and skrollr incomplete

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 
 $('#resume-btn').click(function(){
-	$('.model').removeClass('close');
+	$('.modal').removeClass('close');
 	$('.modal').addClass('active');
+	setTimeout(function() {
+		$('.modal-content').removeClass('remove');
+	},100);
+	
 });
 $('.close-btn').click(function(){
 	$('.modal').removeClass('active');
+	$('.modal-content').addClass('remove');
 	$('.modal').addClass('close');
 });
 
+// INIT SKROLLR PARALLAX
+skrollr.init(
+	{smoothScrolling:true}
+);
